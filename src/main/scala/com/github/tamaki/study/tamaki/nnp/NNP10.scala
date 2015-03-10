a922314336fe49a7e27b780004e3864049eafdda
kohsaku.onozawa@bizreach.co.jp <kohsaku.onozawa@bizreach.co.jp>
2015-03-10 10:26:53 +0900

    no message

--- src/main/scala/com/github/tamaki/study/tamaki/nnp/NNP10.scala
+++ src/main/scala/com/github/tamaki/study/tamaki/nnp/NNP10.scala
@@ -7,32 +7,38 @@ trait NNP10 {
 
   // P01 (*) Find the last element of a list.
   def last(list: List[Int]): Int = {
-    ???
+    list.last
   }
 
   // P02 (*) Find the last but one element of a list.
   def penultimate(list: List[Int]): Int = {
-    ???
+    list.init.last
   }
 
   def nth(n: Int, list: List[Int]): Int = {
-    ???
+    list(n)
   }
 
   def length(list: List[Int]): Int = {
-    ???
+    list.length
   }
 
   def reverse(list: List[Int]): List[Int] = {
-    ???
+    list.reverse
   }
 
   def isPalindrome(list: List[Int]): Boolean = {
-    ???
+    list == list.reverse
   }
 
   def flatten(nested: List[Any]): List[Any] = {
-    ???
+    nested.collect{
+      case l: List[Any] => l.collect{
+        case l2: List[Int] => l2.toSet
+        case i2: Int => Set(i2)
+      }.flatten
+      case i: Int => Set(i)
+    }.flatten
   }
 
   def compress(list: List[Symbol]): List[Symbol] = {
